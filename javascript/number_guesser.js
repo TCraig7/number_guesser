058ed6d42f81084b9443bed805e6c9f5a1f40564
Tara Craig <tara.craig7@gmail.com>
2018-11-19 14:36:56 -0700

    Add let before message to denote a variable.

--- javascript/number_guesser.js
+++ javascript/number_guesser.js
@@ -3,13 +3,13 @@ let guess = null;
 
 function guessChecker() {
   if (guess === randomNumber) {
-    message = 'BOOM!';
+    let message = 'BOOM!';
     $('#message').text(message);
   } else if (guess > randomNumber) {
-    message = 'That is too high';
+    let message = 'That is too high';
     $('#message').text(message);
   } else {
-    message = 'That is too low'
+    let message = 'That is too low'
     $('#message').text(message);
   }
 }

6a43fd89e8c43a0668bcf19c1a78dd82d7eba515
Tara Craig <tara.craig7@gmail.com>
2018-11-20 11:57:20 -0700

    Annotates the first 15 lines of code.

--- javascript/number_guesser.js
+++ javascript/number_guesser.js
@@ -1,19 +1,36 @@
 let maxNumber = 100;
+// creates the variable maxNumber so that the default max number in the range is
+// 100. I used let and const instead of var because I wanted to be more deliberate
+// with my code, and I thought it would be good practice to use the newer concepts;
 let minNumber = 1;
+// set the minNumber variable to 1 so that the default minimum number in the range is 1;
 let randomNumber = Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
+// starts the game by randomly generating a number for the player to guess;
 let guess = null;
+// sets the variable guess so that it can be changed later by the user's guess;
 
 function guessChecker() {
+  // my 'function example' lines are creating functions.
   if (guess === randomNumber) {
+    // starts the conditional. checks to see if the guess is equal to the random number;
     let message = 'BOOM!';
+    // set the variable message to Boom if the guess and random number match;
     $('#message').text(message);
+    // prints the message to the page;
     $('#play-again').slideDown('slow');
+    // jquery to have the play again button appear on the screen if the player wins;
   } else if (guess > randomNumber) {
+    // second piece of the conditional. checks to see if the guess is greater than the random number;
     let message = 'That is too high';
+    // sets the message variable to 'that is too high';
     $('#message').text(message);
+    // prints the message to the page;
   } else {
+    // the last piece of the conditional. checks for all branches that do not match the first two;
     let message = 'That is too low'
+    // sets the message variable to 'that is too low';
     $('#message').text(message);
+    // prints the message to the page;
   }
 }
 
